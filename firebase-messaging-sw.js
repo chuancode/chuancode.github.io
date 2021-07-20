c86e9bf9118fdaf68d37cff19adf8467fa04e7a9
chuan <a0976663511@gmail.com>
2021-07-20 10:22:11 +0800

    firebase webpush

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts('https://www.gstatic.com/firebasejs/5.5.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.9/firebase-messaging.js');
+
+// messagingSenderId.
+firebase.initializeApp({
+  'messagingSenderId': '1045809670811'
+});
+
+// messages.
+const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  /*   
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+      body: 'Background Message body.'
+    }; 
+  */
+
+  // Customize notification here
+  const notificationTitle = payLoad.notification.title;
+  const notificationOptions = {
+    body: payLoad.notification.body,
+    icon: payLoad.notification.icon,
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
